8f7da3098033cd693f4cc51720e78c8e805e8a5b
Vinctdea <vincenzo.deangelis23@gmail.com>
2024-06-18 14:42:42 +0200

    feat: add array data

---
+++ js/main.js
@@ -0,0 +1,56 @@
+// Utilizzando i dati forniti, creare un array di oggetti per rappresentare i membri del team.
+// Ogni membro è caratterizzato dalle seguenti informazioni: nome, ruolo e foto.
+// MILESTONE 0:
+// Creare l’array di oggetti con le informazioni fornite.
+
+const arrDati = [
+    pers1 ={
+        "nome": "Wayne Barnett",
+        "ruolo": "CEO & Founder",
+        "img": "wayne-barnett-founder-ceo.jpg"
+    },
+
+    pers2 ={
+        "nome": "Angela",
+        "ruolo": "Chief Editor",
+        "img": "angela-caroll-chief-editor.jpg"
+    },
+
+    pers3 ={
+        "nome": "Walter Gordon",
+        "ruolo": "office Manager",
+        "img": "walter-gordon-office-manager.jpg"
+    },
+
+    pers4 ={
+        "nome": "Angela Lopez",
+        "ruolo": "Social Media Manager",
+        "img": "angela-lopez-social-media-manager.jpg"
+    },
+
+    pers5 = {
+        "nome": "Scott Estrada",
+        "ruolo": "Developer",
+        "img": "scott-estrada-developer.jpg"
+    },
+
+    pers6 = {
+        "nome": "Barbara Ramos",
+        "ruolo": "Graphic Designer",
+        "img": "barbara-ramos-graphic-designer.jpg"
+    }
+
+];
+
+
+
+
+
+// MILESTONE 1:
+// Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto
+// MILESTONE 2:
+// Stampare le stesse informazioni su DOM sottoforma di stringhe
+// BONUS 1:
+// Trasformare la stringa foto in una immagine effettiva
+// BONUS 2:
+// Organizzare i singoli membri in card/schede
